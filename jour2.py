6125a301dedd79a16db347a42f27df7da7e66731
Jules <julesche08@gmail.com>
2024-12-02 22:31:36 +0100

    essaiDifficile

--- jour2.py
+++ jour2.py
@@ -42,10 +42,49 @@ def checkReport(L):
     return res
 
 
-file = open(r"C:\Users\jules\Documents\Cours Semestre 7\adventofCode\adventOfCode2024\jour2Input.txt","r")
-with file as f:
-    alist = [line.rstrip() for line in f]
-file.close()
+
+
+def checkReportUpdate(L):
+    increasing,decreasing = checkOrder(L)
+    
+    res = False
+    
+    for i in range(len(L)-1):
+        if(increasing and L[i] < L[i+1]):
+            if(L[i+1] >= L[i]+1 and L[i+1] <= L[i] + 3):
+                res = True
+            else :
+                L.remove(L[i])
+                if(checkReport(L)):
+                    return True
+                else:
+                    return False
+        elif (increasing and L[i] >= L[i+1]):
+            L.remove(L[i])
+            if(checkReport(L)):
+                return True
+            else:
+                return False
+            
+        elif(decreasing and L[i] > L[i+1]):
+
+            if(L[i+1] <= L[i]-1 and L[i+1] >= L[i] - 3):
+                res = True
+            else : 
+                L.remove(L[i])
+                if(checkReport(L)):
+                    return True
+                else:
+                    return False  
+        elif (decreasing and L[i] <= L[i+1]):
+            L.remove(L[i])
+            if(checkReport(L)):
+                return True
+            else:
+                return False
+        else:
+            return False
+    return res
 
 def checkReportFile(L):
     res = 0
@@ -53,10 +92,16 @@ def checkReportFile(L):
     for i in range(len(L)):
         x = L[i].split()
         x = [ int(y) for y in x]
-        if(checkReport(x)):
+        if(checkReportUpdate(x)):
             res += 1
     return res
 
+file = open(r"C:\Users\jules\Documents\Cours Semestre 7\advent of code\adventOfCode2024\jour2Input.txt","r")
+with file as f:
+    alist = [line.rstrip() for line in f]
+file.close()
+
 print(checkReportFile(alist))
-        
+
+    
     
